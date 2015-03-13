47b60fda7b3a26799cb1e1d44d04f88bafcbe5eb
Connor Hammond <Connor.hammond@gmail.com>
2015-03-13 12:19:35 -0400

    Adds seed & print methods to models.

--- project/models.py
+++ project/models.py
@@ -3,3 +3,23 @@ import sqlite3
 class Model:
 	def __init__(self):
 		pass
+
+class Board:
+	def __init__(self):
+		self.board = []
+
+	def seed_board(self):
+		for x in range(10):
+			self.board.append(" ~ "*10)
+
+	def print_board(self):
+		ocean = []
+		print('   ', '    '.join([(str(x) + " ") for x in range(1,11)]))
+		x = 65
+		for row in self.board:
+			print(chr(x), ' '.join(row))
+			x+=1
+
+new = Board()
+new.seed_board()
+new.print_board()
--- project/views.py
+++ project/views.py
@@ -1,3 +1,15 @@
+import blessings
+
+t = terminal()
+
 class View:
 	def __init__(self):
 		pass
+
+	def welcome_first(self):
+		print("Welcome to Battleship.  Hope you brought a life-jacket.  Enter player 1 name: ")
+		return input()
+
+	def welcome_second(self):
+		print("Enter Player2 name:  ")
+		return input()
